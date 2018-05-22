e8fafa1adda8cfafa40d83ddf9ee4a6b6cb2c0e6
Black008 <594237224@qq.com>
2018-05-22 18:44:48 +0800

    gyx v1.0

---
+++ src/gyx/Test.java
@@ -0,0 +1,10 @@
+package gyx;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("maingyx");
+    }
+    public void gyx(){
+        System.out.println("gyx");
+    }
+}
