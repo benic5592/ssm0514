826710fa615e35621fc05838aad2110404721b13
17615134238 <17615134238@sina.cn>
2018-05-23 11:10:34 +0800

    1.0版本

---
+++ src/lyj/Test.java
@@ -0,0 +1,11 @@
+package lyj;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("main-刘雨健");
+    }
+
+    public void lyj(){
+        System.out.println("刘雨健");
+    }
+}
