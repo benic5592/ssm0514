44d13544e594676538061584ff93e176983583de
swc054321 <swc054321@163.com>
2018-05-23 11:13:58 +0800

    no comments

---
+++ src/swc/test.java
@@ -0,0 +1,10 @@
+package swc;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("叮叮当当");
+    }
+    public void swc(){
+        System.out.println("孙维辰");
+    }
+}
